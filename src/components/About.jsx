e3baf77c8dd70ac48607dd32f22586fba7e55b85
hamadou diallo <hamadou_diallo@yahoo.com>
2023-11-25 11:25:15 -0500

    cleanups

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,22 +1,24 @@
-import React from 'react';
-import { motion } from 'framer-motion';
-import { styles } from '../styles';
-import { services } from '../constants';
-import { fadeIn, textVariant } from '../utils/motion';
-import { SectionWrapper } from '../hoc';
+import React from "react";
+import { motion } from "framer-motion";
+import { styles } from "../styles";
+import { services } from "../constants";
+import { fadeIn, textVariant } from "../utils/motion";
+import { SectionWrapper } from "../hoc";
 
 const ServiceCard = ({ index, title, icon }) => {
   return (
     <motion.div
-      variants={fadeIn('right', 'spring', 0.5 * index, 0.75)}
-      className="xs:w-[250px] w-full card-gradient p-[1px] rounded-[20px] shadow-card">
+      variants={fadeIn("right", "spring", 0.5 * index, 0.75)}
+      className="xs:w-[250px] w-full card-gradient p-[1px] rounded-[20px] shadow-card"
+    >
       <div
         options={{
           max: 45,
           scale: 1,
           speed: 450,
         }}
-        className="bg-jetLight rounded-[20px] py-5 px-12 min-h-[280px] flex justify-evenly items-center flex-col">
+        className="bg-jetLight rounded-[20px] py-5 px-12 min-h-[280px] flex justify-evenly items-center flex-col"
+      >
         <img src={icon} alt={title} className="w-16 h-16 object-contain" />
         <h3 className="text-taupe text-[18px] font-bold text-center">
           {title}
@@ -35,18 +37,15 @@ const About = () => {
       </motion.div>
 
       <motion.p
-        variants={fadeIn('', '', 0.1, 1)}
-        className="mt-4 text-taupe text-[18px] max-w-3xl leading-[30px]">
-        In the digital realm, I, Hamadou, stand tall,
-        A Full Stack Engineer, ready to answer your call.
-        With expertise honed at Amount and Noble's halls,
-        In React, Vue, and JavaScript, I craft solutions that enthrall.
-
-        Optimizing deployments, problem-solving with flair,
-        I elevate projects with precision and care.
-        Reliable in partnership, in code I find art,
-        Let's connect and give your vision a smart start.
-
+        variants={fadeIn("", "", 0.1, 1)}
+        className="mt-4 text-taupe text-[18px] max-w-3xl leading-[30px]"
+      >
+        In the digital realm, I, Hamadou, stand tall, A Full Stack Engineer,
+        ready to answer your call. With expertise honed at Amount and Noble's
+        halls, In React, Vue, JavaScript, and NodeJS, I craft solutions that
+        enthrall. Optimizing deployments, problem-solving with flair, I elevate
+        projects with precision and care. Reliable in partnership, in code I
+        find art. Let's connect and give your vision a smart start.
       </motion.p>
 
       <div className="mt-20 flex flex-wrap gap-10">
@@ -58,4 +57,4 @@ const About = () => {
   );
 };
 
-export default SectionWrapper(About, 'about');
+export default SectionWrapper(About, "about");
--- src/constants/index.js
+++ src/constants/index.js
@@ -22,249 +22,247 @@ import {
   assetcare,
   movie,
   nyeusi,
-  space,
   flatbush,
   noblelogo,
   amount,
   upwork,
-} from '../assets';
+} from "../assets";
 
 export const navLinks = [
   {
-    id: 'about',
-    title: 'About',
+    id: "about",
+    title: "About",
   },
   {
-    id: 'projects',
-    title: 'Projects',
+    id: "projects",
+    title: "Projects",
   },
   {
-    id: 'contact',
-    title: 'Contact',
+    id: "contact",
+    title: "Contact",
   },
 ];
 
 const services = [
   {
-    title: 'Frontend Developer',
+    title: "Frontend Developer",
     icon: frontend,
   },
   {
-    title: 'Backend Developer',
+    title: "Backend Developer",
     icon: backend,
   },
   {
-    title: 'UI/UX Design',
+    title: "UI/UX Design",
     icon: ux,
   },
   {
-    title: 'Mentor / Team leader',
+    title: "Mentor / Team leader",
     icon: prototyping,
   },
 ];
 
 const technologies = [
   {
-    name: 'HTML 5',
+    name: "HTML 5",
     icon: html,
   },
   {
-    name: 'CSS 3',
+    name: "CSS 3",
     icon: css,
   },
   {
-    name: 'JavaScript',
+    name: "JavaScript",
     icon: javascript,
   },
   {
-    name: 'TypeScript',
+    name: "TypeScript",
     icon: typescript,
   },
   {
-    name: 'React JS',
+    name: "React JS",
     icon: reactjs,
   },
   {
-    name: 'Redux Toolkit',
+    name: "Redux Toolkit",
     icon: redux,
   },
   {
-    name: 'Tailwind CSS',
+    name: "Tailwind CSS",
     icon: tailwind,
   },
   {
-    name: 'Node JS',
+    name: "Node JS",
     icon: nodejs,
   },
   {
-    name: 'Rails',
+    name: "Rails",
     icon: rubyrails,
   },
   {
-    name: 'graphql',
+    name: "graphql",
     icon: graphql,
   },
   {
-    name: 'postgresql',
+    name: "postgresql",
     icon: postgresql,
   },
   {
-    name: 'git',
+    name: "git",
     icon: git,
   },
   {
-    name: 'figma',
+    name: "figma",
     icon: figma,
   },
   {
-    name: 'docker',
+    name: "docker",
     icon: docker,
   },
 ];
 
 const experiences = [
   {
-    title: 'Front-End Developer',
-    company_name: 'Flatbush YMCA',
+    title: "Front-End Developer",
+    company_name: "Flatbush YMCA",
     icon: flatbush,
-    iconBg: '#333333',
-    date: 'Aug 2014 - Apr 2016',
+    iconBg: "#333333",
+    date: "Aug 2014 - Apr 2016",
   },
   {
-    title: 'Full Stack Engineer',
-    company_name: 'NobleIndustries',
+    title: "Full Stack Engineer",
+    company_name: "NobleIndustries",
     icon: noble,
-    iconBg: '#333333',
-    date: 'June 2020 - Nov 2021',
+    iconBg: "#333333",
+    date: "June 2020 - Nov 2021",
   },
   {
-    title: 'Full Stack Engineer',
-    company_name: 'Amount',
+    title: "Full Stack Engineer",
+    company_name: "Amount",
     icon: amount,
-    iconBg: '#333333',
-    date: 'June 2022 - Oct 2023',
+    iconBg: "#333333",
+    date: "June 2022 - Oct 2023",
   },
   {
-    title: 'Full Stack Engineer (Freelancer)',
-    company_name: 'Upwork',
+    title: "Full Stack Engineer (Freelancer)",
+    company_name: "Upwork",
     icon: upwork,
-    iconBg: '#333333',
-    date: 'Oct 2023 - Present',
+    iconBg: "#333333",
+    date: "Oct 2023 - Present",
   },
 ];
 
 const projects = [
   {
-    id: 'project-1',
-    name: 'Noble',
-    description: 'Application is designed for ecommerce purpose with the ability to sell merchandises',
+    id: "project-1",
+    name: "Noble",
+    description:
+      "Application is designed for ecommerce purpose with the ability to sell merchandises",
     tags: [
       {
-        name: 'react',
-        color: 'blue-text-gradient',
+        name: "react",
+        color: "blue-text-gradient",
       },
       {
-        name: 'mongodb',
-        color: 'green-text-gradient',
+        name: "mongodb",
+        color: "green-text-gradient",
       },
       {
-        name: 'tailwind',
-        color: 'pink-text-gradient',
+        name: "tailwind",
+        color: "pink-text-gradient",
       },
       {
-        name: 'AWS',
-        color: 'red-text-gradient',
+        name: "AWS",
+        color: "red-text-gradient",
       },
       {
-        name: 'PostgreSQL',
-        color: 'gray-text-gradient',
+        name: "PostgreSQL",
+        color: "gray-text-gradient",
       },
     ],
     image: noblelogo,
-    repo: 'https://github.com/morpheus86/ndstry',
-    demo: 'https://ndstry.vercel.app/'
+    repo: "https://github.com/morpheus86/ndstry",
   },
   {
-    id: 'project-2',
-    name: 'Clonebnb',
+    id: "project-2",
+    name: "Clonebnb",
     description:
-      'Application allows us to create a user, be able to add a house, its descriptions, upload the house pictures, keep track of the houses you have and the house you have booked.',
+      "Mobile first Web Application that allows us to create a user, be able to add a house, its descriptions, upload the house pictures, keep track of the houses you have and the house you have booked.",
     tags: [
       {
-        name: 'react',
-        color: 'blue-text-gradient',
+        name: "react",
+        color: "blue-text-gradient",
       },
       {
-        name: 'restapi',
-        color: 'green-text-gradient',
+        name: "restapi",
+        color: "green-text-gradient",
       },
       {
-        name: 'scss',
-        color: 'pink-text-gradient',
+        name: "scss",
+        color: "pink-text-gradient",
       },
     ],
     image: clonebnb,
-    repo: 'https://github.com/morpheus86/clonebnb',
-    demo: 'https://clonebnb.now.sh/'
+    repo: "https://github.com/morpheus86/clonebnb",
   },
   {
-    id: 'project-3',
-    name: 'Assetcare',
-    description: 'This is a single-page calculator app built with React',
+    id: "project-3",
+    name: "Assetcare",
+    description: "This is a single-page calculator app built with React",
     tags: [
       {
-        name: 'C#',
-        color: 'blue-text-gradient',
+        name: "C#",
+        color: "blue-text-gradient",
       },
       {
-        name: '.NET',
-        color: 'green-text-gradient',
+        name: ".NET",
+        color: "green-text-gradient",
       },
       {
-        name: 'React',
-        color: 'pink-text-gradient',
+        name: "React",
+        color: "pink-text-gradient",
       },
     ],
     image: assetcare,
-    repo: 'https://github.com/morpheus86/AssetCare.git',
+    repo: "https://github.com/morpheus86/AssetCare.git",
   },
   {
-    id: 'project-4',
-    name: 'Movie Metro',
+    id: "project-4",
+    name: "Movie Metro",
     description: `A single-page application that allows users to search for any movie or show's ratings and its details.`,
     tags: [
       {
-        name: 'nextjs',
-        color: 'blue-text-gradient',
+        name: "nextjs",
+        color: "blue-text-gradient",
       },
       {
-        name: 'supabase',
-        color: 'green-text-gradient',
+        name: "supabase",
+        color: "green-text-gradient",
       },
       {
-        name: 'css',
-        color: 'pink-text-gradient',
+        name: "css",
+        color: "pink-text-gradient",
       },
     ],
     image: movie,
   },
   {
-    id: 'project-5',
-    name: 'Nyeusi Fest Site',
+    id: "project-5",
+    name: "Nyeusi Fest Site",
     description:
-      'This is a demo concert website for a music festival called Nyeusi.',
+      "This is a demo concert website for a music festival called Nyeusi.",
     tags: [
       {
-        name: 'nextjs',
-        color: 'blue-text-gradient',
+        name: "nextjs",
+        color: "blue-text-gradient",
       },
       {
-        name: 'supabase',
-        color: 'green-text-gradient',
+        name: "supabase",
+        color: "green-text-gradient",
       },
       {
-        name: 'css',
-        color: 'pink-text-gradient',
+        name: "css",
+        color: "pink-text-gradient",
       },
     ],
     image: nyeusi,
