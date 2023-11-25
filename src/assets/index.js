8d92a53981366682e2e6265b2e4049a6851c449e
hamadou diallo <hamadou_diallo@yahoo.com>
2023-11-25 11:37:14 -0500

    cleanups

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,55 +1,55 @@
-import bwmap from './backgrounds/bw-map.jpeg';
-import nairobi from './backgrounds/nairobi.png';
-import whiteabstract from './backgrounds/white-abstract.png';
-import worldmap from './backgrounds/world-map.png';
+import bwmap from "./backgrounds/bw-map.jpeg";
+import nairobi from "./backgrounds/nairobi.png";
+import whiteabstract from "./backgrounds/white-abstract.png";
+import worldmap from "./backgrounds/world-map.png";
 
-import logo from './logo/logo-black.png';
-import logotext from './logo/logo-text-black.png';
+import logo from "./logo/logo-black.png";
+import logotext from "./logo/logo-text-black.png";
 
-import Hamadou from './personal/Hamadou.png';
+import Hamadou from "./personal/Hamadou.png";
 
-import backend from './icons/backend.png';
-import ux from './icons/ux.png';
-import frontend from './icons/frontend.png';
-import prototyping from './icons/prototyping.png';
-import github from './icons/github.png';
-import close from './icons/close.png';
-import download from './icons/download.png';
-import downloadHover from './icons/downloadHover.png';
-import pineapple from './icons/pineapple.png';
-import pineappleHover from './icons/pineappleHover.png';
-import menu from './icons/menu.png';
-import resume from './icons/resume.png';
-import send from './icons/send.png';
-import sendHover from './icons/sendHover.png';
+import backend from "./icons/backend.png";
+import ux from "./icons/ux.png";
+import frontend from "./icons/frontend.png";
+import prototyping from "./icons/prototyping.png";
+import github from "./icons/github.png";
+import close from "./icons/close.png";
+import download from "./icons/download.png";
+import downloadHover from "./icons/downloadHover.png";
+import pineapple from "./icons/pineapple.png";
+import pineappleHover from "./icons/pineappleHover.png";
+import menu from "./icons/menu.png";
+import resume from "./icons/resume.png";
+import send from "./icons/send.png";
+import sendHover from "./icons/sendHover.png";
 
-import css from './tech/css.png';
-import docker from './tech/docker.png';
-import figma from './tech/figma.png';
-import git from './tech/git.png';
-import html from './tech/html.png';
-import javascript from './tech/javascript.png';
-import nodejs from './tech/nodejs.png';
-import reactjs from './tech/reactjs.png';
-import redux from './tech/redux.png';
-import tailwind from './tech/tailwind.png';
-import typescript from './tech/typescript.png';
-import graphql from './tech/graphql.png';
-import postgresql from './tech/postgresql.png';
-import rubyrails from './tech/rubyrails.png';
+import css from "./tech/css.png";
+import docker from "./tech/docker.png";
+import figma from "./tech/figma.png";
+import git from "./tech/git.png";
+import html from "./tech/html.png";
+import javascript from "./tech/javascript.png";
+import nodejs from "./tech/nodejs.png";
+import reactjs from "./tech/reactjs.png";
+import redux from "./tech/redux.png";
+import tailwind from "./tech/tailwind.png";
+import typescript from "./tech/typescript.png";
+import graphql from "./tech/graphql.png";
+import postgresql from "./tech/postgresql.png";
+import rubyrails from "./tech/rubyrails.png";
 
-import flatbush from './company/flatbush.png';
-import noble from './company/noble.png';
-import amount from './company/amount.png';
-import upwork from './company/upwork.png';
+import flatbush from "./company/flatbush.png";
+import noble from "./company/noble.png";
+import amount from "./company/amount.png";
+import upwork from "./company/upwork.png";
 
-import noblelogo from './projects/noble.png';
+import noblelogo from "./projects/noble.png";
 
-import clonebnb from './projects/clonebnb.png';
-import assetcare from './projects/assetcare.png';
-import movie from './projects/movie-metro.png';
-import nyeusi from './projects/nyeusi.png';
-import space from './projects/space-hub.png';
+import clonebnb from "./projects/clonebnb.png";
+import assetcare from "./projects/assetcare.png";
+import movie from "./projects/movie-metro.png";
+import nyeusi from "./projects/nyeusi.png";
+import space from "./projects/space-hub.png";
 
 export {
   bwmap,
