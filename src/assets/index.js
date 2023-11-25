c740cc5c3cde4de016b20ee48196506642751655
hamadou diallo <hamadou_diallo@yahoo.com>
2023-11-25 12:34:14 -0500

    build

--- src/assets/index.js
+++ src/assets/index.js
@@ -6,7 +6,7 @@ import worldmap from "./backgrounds/world-map.png";
 import logo from "./logo/logo-black.png";
 import logotext from "./logo/logo-text-black.png";
 
-import Hamadou from "./personal/Hamadou.png";
+import hamadou from "./personal/hamadou.png";
 
 import backend from "./icons/backend.png";
 import ux from "./icons/ux.png";
@@ -58,7 +58,7 @@ export {
   worldmap,
   logo,
   logotext,
-  Hamadou,
+  hamadou,
   backend,
   ux,
   frontend,
